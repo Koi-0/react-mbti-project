d2898bd73466931fb0b59dbb5ff836ab93dd7c39
Koi-0 <dndud5182@gmail.com>
2025-02-20 18:02:49 +0900

    Feat : Route 생성 후 path 설정 및 element 연결 완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,24 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
+import ProfilePage from "./pages/ProfilePage";
+import TestPage from "./pages/TestPage";
+import TestResultPage from "./pages/TestResultPage";
+
 const App = () => {
-    return <div className="text-red-700">App</div>;
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/loginPage" element={<LoginPage />} />
+                <Route path="/signupPage" element={<SignupPage />} />
+                <Route path="/profilePage" element={<ProfilePage />} />
+                <Route path="/testPage" element={<TestPage />} />
+                <Route path="/testResultPage" element={<TestResultPage />} />
+            </Routes>
+        </BrowserRouter>
+    );
 };
 
 export default App;
