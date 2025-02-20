0cb2e273cf6ad7211bb3d9eea8eb0acbb9ca1fe5
Koi-0 <dndud5182@gmail.com>
2025-02-20 19:55:14 +0900

    Remove : Link 태그 제거

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,7 @@
-import { Link } from "react-router-dom";
 import TestPage from "./TestPage";
 
 const HomePage = () => {
-    return (
-        <Link to={"/loginPage"}>
-            <TestPage />
-        </Link>
-    );
+    return <TestPage />;
 };
 
 export default HomePage;
