cf42d55e02e4ecbc748cb8f7c3d9813fac1ea029
Koi-0 <dndud5182@gmail.com>
2025-02-20 20:22:38 +0900

    Style : css 수정 및 추가

--- src/styles/GlobalStyle.jsx
+++ src/styles/GlobalStyle.jsx
@@ -6,10 +6,19 @@ const GlobalStyle = createGlobalStyle`
     padding: 0;
     box-sizing: border-box;
 }
+
 a {
     font-weight: bold;
     color: #ff5a5f;
     text-decoration: inherit;
+}
+
+button {
+    background-color: #ff5a5f;
+    transition: background-color 0.3s;
+    &:hover {
+        background-color: transparent;
+    }
 }
 `;
 
