029066c43306b35e47315c64071690057b1fd3f0
Koi-0 <dndud5182@gmail.com>
2025-02-20 19:42:27 +0900

    Feat : Header 컴포넌트 공통 적용

--- src/App.jsx
+++ src/App.jsx
@@ -6,19 +6,23 @@ import ProfilePage from "./pages/ProfilePage";
 import TestPage from "./pages/TestPage";
 import TestResultPage from "./pages/TestResultPage";
 import GlobalStyle from "./styles/GlobalStyle";
+import Header from "./components/Header";
 
 const App = () => {
     return (
         <BrowserRouter>
             <GlobalStyle />
-            <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/loginPage" element={<LoginPage />} />
-                <Route path="/signupPage" element={<SignupPage />} />
-                <Route path="/profilePage" element={<ProfilePage />} />
-                <Route path="/testPage" element={<TestPage />} />
-                <Route path="/testResultPage" element={<TestResultPage />} />
-            </Routes>
+            <div className="h-full bg-gray-100 flex flex-col justify-between">
+                <Header />
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/loginPage" element={<LoginPage />} />
+                    <Route path="/signupPage" element={<SignupPage />} />
+                    <Route path="/profilePage" element={<ProfilePage />} />
+                    <Route path="/testPage" element={<TestPage />} />
+                    <Route path="/testResultPage" element={<TestResultPage />} />
+                </Routes>
+            </div>
         </BrowserRouter>
     );
 };
