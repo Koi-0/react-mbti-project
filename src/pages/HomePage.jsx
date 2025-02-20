dd621229261052f47a0782b0b9da88ac87de7919
Koi-0 <dndud5182@gmail.com>
2025-02-20 19:43:38 +0900

    Remove : 기존 Header 컴포넌트 제거

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,15 +1,11 @@
 import { Link } from "react-router-dom";
-import Header from "../components/Header";
 import TestPage from "./TestPage";
 
 const HomePage = () => {
     return (
-        <div className="h-full bg-gray-100 flex flex-col justify-between">
-            <Header />
-            <Link to={"/loginPage"}>
-                <TestPage />
-            </Link>
-        </div>
+        <Link to={"/loginPage"}>
+            <TestPage />
+        </Link>
     );
 };
 
