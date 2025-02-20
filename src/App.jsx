d54b3453a6c468dfe628b847f43ecccbba05cfee
Koi-0 <dndud5182@gmail.com>
2025-02-20 21:29:58 +0900

    Remove : GlobalStyle 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,11 @@ import SignupPage from "./pages/SignupPage";
 import ProfilePage from "./pages/ProfilePage";
 import TestPage from "./pages/TestPage";
 import TestResultPage from "./pages/TestResultPage";
-import GlobalStyle from "./styles/GlobalStyle";
 import Header from "./components/Header";
 
 const App = () => {
     return (
         <BrowserRouter>
-            <GlobalStyle />
             <div className="h-full bg-gray-100 flex flex-col justify-between">
                 <Header />
                 <main className="container mx-auto pt-10 main">
