eda7cb9d10f5ae3c8ab87e120ef7206aa718659d
Koi-0 <dndud5182@gmail.com>
2025-02-20 19:29:44 +0900

    Feat : TestPage 컴포넌트 연결

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,15 @@
 import { Link } from "react-router-dom";
+import Header from "../components/Header";
+import TestPage from "./TestPage";
 
 const HomePage = () => {
     return (
-        <>
+        <div className="h-full bg-gray-100 flex flex-col justify-between">
+            <Header />
             <Link to={"/loginPage"}>
-                <div>HomePage</div>
+                <TestPage />
             </Link>
-        </>
+        </div>
     );
 };
 
