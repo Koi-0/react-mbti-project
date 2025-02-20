26a53f9e814dafd07054ce4a1df483f7001555cb
Koi-0 <dndud5182@gmail.com>
2025-02-20 19:46:33 +0900

    Feat : className 추가 및 tailwind css 적용

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,20 @@
+import { Link } from "react-router-dom";
+
 const Header = () => {
-    return <div>Header</div>;
+    return (
+        <header className="bg-black p-4">
+            <nav className="container mx-auto flex justify-between items-center text-white">
+                <Link to={"/"} className="hover:text-gray-300">
+                    홈
+                </Link>
+                <div className="space-x-4">
+                    <Link to={"/loginPage"} className="hover:text-gray-300">
+                        로그인
+                    </Link>
+                </div>
+            </nav>
+        </header>
+    );
 };
 
 export default Header;
