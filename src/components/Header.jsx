b4ed9ebbe7933bd3df5e906001fb719fbb1a2531
Young <dndud5182@gmail.com>
2025-02-21 21:10:58 +0900

    Feat : Outlet 적용

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,22 @@
-import { Link } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 
 const Header = () => {
     return (
-        <header className="bg-gray-100 p-4 shadow-md">
-            <nav className="container mx-auto flex justify-between items-center">
-                <Link to={"/"} className="text-primary">
-                    홈
-                </Link>
-                <div>
-                    <Link to={"/loginPage"} className="text-primary">
-                        로그인
+        <div className="w-screen h-screen flex flex-col justify-between">
+            <header className="bg-gray-100 p-4 drop-shadow-md">
+                <nav className="container mx-auto flex justify-between items-center">
+                    <Link to={"/"} className="text-primary">
+                        홈
                     </Link>
-                </div>
-            </nav>
-        </header>
+                    <div>
+                        <Link to={"/loginPage"} className="text-primary">
+                            로그인
+                        </Link>
+                    </div>
+                </nav>
+            </header>
+            <Outlet />
+        </div>
     );
 };
 
