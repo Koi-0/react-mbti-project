5aba037ea70598222491cee85c4d5d8effcc8b4d
Young <dndud5182@gmail.com>
2025-02-21 19:34:32 +0900

    Style : @layer 적용

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,11 +3,11 @@ import { Link } from "react-router-dom";
 const Header = () => {
     return (
         <header className="bg-gray-100 p-4 shadow-md">
-            <nav className="container mx-auto flex justify-between items-center text-white">
-                <Link to={"/"} className="text-primary text-lg">
+            <nav className="container mx-auto flex justify-between items-center">
+                <Link to={"/"} className="text-primary">
                     홈
                 </Link>
-                <div className="space-x-4">
+                <div>
                     <Link to={"/loginPage"} className="text-primary">
                         로그인
                     </Link>
