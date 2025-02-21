d5297a6a7b15627cc291ecb4a62aa5d99dea2691
Young <dndud5182@gmail.com>
2025-02-21 20:31:42 +0900

    Feat : Router 컴포넌트 연결

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,7 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import HomePage from "./pages/HomePage";
-import LoginPage from "./pages/LoginPage";
-import SignupPage from "./pages/SignupPage";
-import ProfilePage from "./pages/ProfilePage";
-import TestPage from "./pages/TestPage";
-import TestResultPage from "./pages/TestResultPage";
-import Header from "./components/Header";
+import Router from "./shared/router";
 
 const App = () => {
-    return (
-        <BrowserRouter>
-            <Header />
-            <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/loginPage" element={<LoginPage />} />
-                <Route path="/signupPage" element={<SignupPage />} />
-                <Route path="/profilePage" element={<ProfilePage />} />
-                <Route path="/testPage" element={<TestPage />} />
-                <Route path="/testResultPage" element={<TestResultPage />} />
-            </Routes>
-        </BrowserRouter>
-    );
+    return <Router />;
 };
 
 export default App;
