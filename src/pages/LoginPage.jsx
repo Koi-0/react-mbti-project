e74bd220713394a48bc61929fbcf81f5c20666fa
Young <dndud5182@gmail.com>
2025-02-22 13:06:25 +0900

    Feat : AuthForm 컴포넌트에 type 추가

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,7 +1,26 @@
+import { Link } from "react-router-dom";
 import AuthForm from "../components/AuthForm";
 
 const LoginPage = () => {
-    return <AuthForm />;
+    return (
+        <div className="w-full flex flex-col items-center justify-center bg-gray-100">
+            <div className="w-full bg-white shadow-lg rounded-lg p-8 max-w-md">
+                <h1 className="h2-primary text-3xl mb-6">로그인</h1>
+                <AuthForm type="login" />
+                <div className="mt-4">
+                    <p className="text-gray-700">
+                        계정이 없으신가요?
+                        <Link
+                            to={"/signupPage"}
+                            className="text-[#FF5A5F] hover:underline ml-3"
+                        >
+                            회원가입
+                        </Link>
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default LoginPage;
--- src/pages/SignupPage.jsx
+++ src/pages/SignupPage.jsx
@@ -1,7 +1,26 @@
+import { Link } from "react-router-dom";
 import AuthForm from "../components/AuthForm";
 
 const SignupPage = () => {
-    return <AuthForm />;
+    return (
+        <div className="w-full flex flex-col items-center justify-center bg-gray-100">
+            <div className="w-full bg-white shadow-lg rounded-lg p-8 max-w-md">
+                <h1 className="h2-primary text-3xl mb-6">회원가입</h1>
+                <AuthForm type="signup" />
+                <div className="mt-4">
+                    <p className="text-gray-700">
+                        이미 계정이 있으신가요?
+                        <Link
+                            to={"/loginPage"}
+                            className="text-[#FF5A5F] hover:underline ml-3"
+                        >
+                            로그인
+                        </Link>
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default SignupPage;
