3c0e7d6152dde5297e94c90951d5b3a90a622594
Young <dndud5182@gmail.com>
2025-02-22 17:55:40 +0900

    Feat : 회원가입 로직에 try.catch 추가

--- src/api/auth.js
+++ src/api/auth.js
@@ -3,12 +3,16 @@ import axios from "axios";
 const API_URL = "https://www.nbcamp-react-auth.link";
 
 export const register = async (userData) => {
-    const response = await axios.post(`${API_URL}/register`, userData);
-    return response.data;
+    try {
+        const response = await axios.post(`${API_URL}/register`, userData);
+        return response.data;
+    } catch (error) {
+        console.log("error =>", error);
+    }
 };
 
-export const login = async (userData) => {};
+// export const login = async (userData) => {};
 
-export const getUserProfile = async (token) => {};
+// export const getUserProfile = async (token) => {};
 
-export const updateProfile = async (formData) => {};
+// export const updateProfile = async (formData) => {};
