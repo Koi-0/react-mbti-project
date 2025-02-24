5d891b500865b55f43d064af06ebfb9aa22c5a7f
Young <dndud5182@gmail.com>
2025-02-24 18:28:05 +0900

    Feat : 회원가입, 로그인 페이지의 이벤트 연결 완료 [onSubmitHandler, onChangeHandler]

--- src/components/AuthForm.jsx
+++ src/components/AuthForm.jsx
@@ -1,24 +1,36 @@
-const AuthForm = ({ type }) => {
+const AuthForm = ({ type, onSubmitHandler, onChangeHandler }) => {
     return (
-        <form className="space-y-6 bg-gray-50 p-6 rounded-lg shadow-md">
+        <form
+            onSubmit={onSubmitHandler}
+            className="space-y-6 bg-gray-50 p-6 rounded-lg shadow-md"
+        >
             <input
                 type="text"
                 name="id"
-                placeholder="아이디"
+                placeholder="아이디 (4글자 ~ 10글자)"
                 className="input-primary"
+                minLength={4}
+                maxLength={10}
+                onChange={onChangeHandler}
             />
             <input
                 type="text"
                 name="password"
-                placeholder="비밀번호"
+                placeholder="비밀번호 (4글자 ~ 15글자)"
                 className="input-primary"
+                minLength={4}
+                maxLength={15}
+                onChange={onChangeHandler}
             />
             {type === "signup" && (
                 <input
                     type="text"
                     name="nickname"
-                    placeholder="닉네임"
+                    placeholder="닉네임 (1글자 ~ 10글자)"
                     className="input-primary"
+                    minLength={1}
+                    maxLength={10}
+                    onChange={onChangeHandler}
                 />
             )}
             <button
